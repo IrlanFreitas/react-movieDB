1bb213951fef57b702d32fc432a6038602986aa7
Irlan <irlan.junior@solutis.com.br>
2020-11-13 19:47:05 -0300

    Componente Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from './components/Header/Header'
+import Home from './views/Home'
 import { GlobalStyle } from './GlobalStyle'
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Home />
       <GlobalStyle />
       Start here.
     </div>
