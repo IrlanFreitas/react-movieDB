860b006e9ce5740c3ff4a5998926526862d53d7e
Irlan <irlan.junior@solutis.com.br>
2020-11-13 19:49:05 -0300

    Merge

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import RMDBLogo from '../../assets/images/react-movie-logo.svg'
+import TMDBLogo from '../../assets/images/tmdb_logo.svg'
+
+import { Wrapper, Content, LogoImg, TMDBLogoImg } from './styles.js'
+
+export default function Header() {
+    return (<Wrapper>
+        <Content>
+            <LogoImg src={RMDBLogo} alt="rmdb-logo" />
+            <TMDBLogoImg src={TMDBLogo} alt="tmdb-logo" />
+        </Content>
+    </Wrapper>
+    )
+}
+
+// ! Outras formas de fazer
+// export const Header1 = () => (<Wrapper>
+//     <Content>
+//         <LogoImg src={RMDBLogo} alt="rmdb-logo" />
+//         <TMDBLogoImg src={TMDBLogo} alt="tmdb-logo" />
+//     </Content>
+// </Wrapper>)
+
+// * Outros componentes dentro do arquivo
+
+// * Outros componentes dentro do arquivo
+// export const Header2 = () => (<Wrapper>
+//     <Content>
+//         <LogoImg src={RMDBLogo} alt="rmdb-logo" />
+//         <TMDBLogoImg src={TMDBLogo} alt="tmdb-logo" />
+//     </Content>
+// </Wrapper>)
+
+// ? Exportando
+//TODO dspajdasd
+// export default Header;
